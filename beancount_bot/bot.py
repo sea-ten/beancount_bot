d9383a8abb60c7197e010ac52e1a1d3fbfdf0758
sea-ten <58674672+sea-ten@users.noreply.github.com>
2025-09-28 10:28:29 +0800

    Update bot.py

--- beancount_bot/bot.py
+++ beancount_bot/bot.py
@@ -1,321 +1,242 @@
-import telebot
-from telebot import apihelper
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, MessageEntity, Message, CallbackQuery
+import logging
+import asyncio
 
-from beancount_bot import transaction
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, MessageEntity
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
+
+from beancount_bot import transaction, config as conf, __VERSION__
 from beancount_bot.config import get_config, load_config
 from beancount_bot.dispatcher import Dispatcher
 from beancount_bot.i18n import _
-from beancount_bot.session import get_session, SESS_AUTH, get_session_for, set_session, SESS_TX_TAGS
+from beancount_bot.session import get_session, SESS_AUTH, set_session, SESS_TX_TAGS, load_session
 from beancount_bot.session_config import SESSION_CONFIG
 from beancount_bot.task import load_task, get_task
 from beancount_bot.transaction import get_manager
-from beancount_bot.util import logger
 
-apihelper.ENABLE_MIDDLEWARE = True
-
-bot = telebot.TeleBot(token=None, parse_mode=None)
-
-
-@bot.middleware_handler(update_types=['message'])
-def session_middleware(bot_instance, message):
-    """
-    会话中间件
-    :param bot_instance:
-    :param message:
-    :return:
-    """
-    bot_instance.session_user_id = message.from_user.id
-    bot_instance.session = get_session_for(message.from_user.id)
+# 设置日志
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 
 #######
 # 鉴权 #
 #######
 
-def check_auth() -> bool:
-    """
-    检查是否登录
-    :return:
-    """
-    return SESS_AUTH in bot.session and bot.session[SESS_AUTH]
+def check_auth(user_id: int) -> bool:
+    """检查是否登录"""
+    session = get_session(user_id, SESS_AUTH, False)
+    return session
 
 
-@bot.message_handler(commands=['start'])
-def start_handler(message: Message):
-    """
-    首次聊天时鉴权
-    :param message:
-    :return:
-    """
-    auth = get_session(message.from_user.id, SESS_AUTH, False)
-    if auth:
-        bot.reply_to(message, _("已经鉴权过了！"))
-        return
-    # 要求鉴权
-    bot.reply_to(message, _("欢迎使用记账机器人！请输入鉴权令牌："))
-
-
-def auth_token_handler(message: Message):
-    """
-    登陆令牌回调
-    :param message:
-    :return:
-    """
-    if check_auth():
-        return
-    # 未鉴权则视为鉴权令牌
-    auth_token = get_config('bot.auth_token')
-    if auth_token == message.text:
-        set_session(message.from_user.id, SESS_AUTH, True)
-        bot.reply_to(message, _("鉴权成功！"))
+async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """首次聊天时鉴权"""
+    if check_auth(update.effective_user.id):
+        await update.message.reply_text(_("已经鉴权过了！"))
     else:
-        bot.reply_to(message, _("鉴权令牌错误！"))
+        await update.message.reply_text(_("欢迎使用记账机器人！请输入鉴权令牌："))
 
 
 #######
 # 指令 #
 #######
 
-
-@bot.message_handler(commands=['reload'])
-def reload_handler(message):
-    """
-    重载配置指令
-    :param message:
-    :return:
-    """
-    if not check_auth():
-        bot.reply_to(message, _("请先进行鉴权！"))
+async def reload_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """重载配置指令"""
+    if not check_auth(update.effective_user.id):
+        await update.message.reply_text(_("请先进行鉴权！"))
         return
     load_config()
+    # 重新加载任务定义，但不需要重新启动调度线程
     load_task()
-    bot.reply_to(message, _("成功重载配置！"))
+    # 注意：这里没有重新启动 JobQueue 的逻辑，因为这比较复杂。
+    # 对于定时任务的修改，建议重启机器人。
+    await update.message.reply_text(_("成功重载配置！"))
 
 
-@bot.message_handler(commands=['help'])
-def help_handler(message):
-    """
-    帮助指令
-    :param message:
-    :return:
-    """
-    cmd = message.text
-    dispatchers = get_manager().dispatchers
-    if cmd == '/help':
-        # 创建消息按键
-        markup = InlineKeyboardMarkup()
-        for ind, d in zip(range(len(dispatchers)), dispatchers):
-            help_btn = _("帮助：{name}").format(name=d.get_name())
-            markup.add(InlineKeyboardButton(help_btn, callback_data=f'help:{ind}'))
-        # 帮助信息
-        command_usage = [
-            _("/start - 鉴权"),
-            _("/help - 使用帮助"),
-            _("/reload - 重新加载配置文件"),
-            _("/task - 查看、运行任务"),
-            _("/set - 设置用户特定配置"),
-            _("/get - 获取用户特定配置"),
-        ]
-        help_text = \
-            _("记账 Bot\n\n可用指令列表：\n{command}\n\n交易语句语法帮助请选择对应模块，或使用 /help [模块名] 查看。").format(
-                command='\n'.join(command_usage))
-        bot.reply_to(message, help_text, reply_markup=markup)
-    else:
-        # 显示详细帮助
-        name: str = cmd[6:]
-        flag_found = False
-        for d in dispatchers:
-            if name.lower() == d.get_name().lower():
-                show_usage_for(message, d)
-                flag_found = True
-        if not flag_found:
-            bot.reply_to(message, _("对应名称的交易语句处理器不存在！"))
-
-
-def show_usage_for(message: Message, d: Dispatcher):
-    """
-    显示特定处理器的使用方法
-    :param message:
-    :param d:
-    :return:
-    """
+async def show_usage_for(message, d: Dispatcher):
+    """显示特定处理器的使用方法"""
     usage = _("帮助：{name}\n\n{usage}").format(name=d.get_name(), usage=d.get_usage())
-    bot.reply_to(message, usage)
+    await message.reply_text(usage)
 
 
-@bot.callback_query_handler(func=lambda call: call.data[:4] == 'help')
-def callback_help(call: CallbackQuery):
-    """
-    帮助语句详细帮助的回调
-    :param call:
-    :return:
-    """
+async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """帮助指令"""
+    dispatchers = get_manager().dispatchers
+    if not context.args:
+        buttons = [[InlineKeyboardButton(_("帮助：{name}").format(name=d.get_name()), callback_data=f'help:{i}')]
+                   for i, d in enumerate(dispatchers)]
+        markup = InlineKeyboardMarkup(buttons)
+        command_usage = [
+            _("/start - 鉴权"), _("/help - 使用帮助"), _("/reload - 重新加载配置文件"),
+            _("/task - 查看、运行任务"), _("/set - 设置用户特定配置"), _("/get - 获取用户特定配置"),
+        ]
+        help_text = _("记账 Bot\n\n可用指令列表：\n{command}\n\n交易语句语法帮助请选择对应模块，或使用 /help [模块名] 查看。").format(
+            command='\n'.join(command_usage))
+        await update.message.reply_text(help_text, reply_markup=markup)
+    else:
+        name = ' '.join(context.args)
+        d = next((d for d in dispatchers if name.lower() == d.get_name().lower()), None)
+        if d:
+            await show_usage_for(update.message, d)
+        else:
+            await update.message.reply_text(_("对应名称的交易语句处理器不存在！"))
+
+
+async def callback_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """帮助回调"""
+    query = update.callback_query
+    await query.answer()
     try:
-        d_id = int(call.data[5:])
+        d_id = int(query.data[5:])
         dispatchers = get_manager().dispatchers
-        show_usage_for(call.message, dispatchers[d_id])
-    except Exception as e:
-        logger.error(f'{call.id}：发生未知错误！', exc_info=e)
-        bot.answer_callback_query(call.id, _("发生未知错误！"))
+        await show_usage_for(query.message, dispatchers[d_id])
+    except (ValueError, IndexError) as e:
+        logger.error(f'{query.id}：帮助回调发生错误！', exc_info=e)
+        await query.answer(_("发生未知错误！"), show_alert=True)
 
 
-@bot.message_handler(commands=['task'])
-def task_handler(message):
-    """
-    任务指令
-    :param message:
-    :return:
-    """
-    if not check_auth():
-        bot.reply_to(message, _("请先进行鉴权！"))
+async def task_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """任务指令"""
+    if not check_auth(update.effective_user.id):
+        await update.message.reply_text(_("请先进行鉴权！"))
         return
-
-    cmd = message.text
     tasks = get_task()
-    if cmd == '/task':
-        # 显示所有任务
+    if not context.args:
         all_tasks = ', '.join(tasks.keys())
-        bot.reply_to(message,
-                     _("当前注册任务：{all_tasks}\n"
-                       "可以通过 /task [任务名] 主动触发").format(all_tasks=all_tasks))
+        await update.message.reply_text(_("当前注册任务：{all_tasks}\n可以通过 /task [任务名] 主动触发").format(all_tasks=all_tasks))
     else:
-        # 运行任务
-        dest = cmd[6:]
-        if dest not in tasks:
-            bot.reply_to(message, _("任务不存在！"))
+        dest = ' '.join(context.args)
+        task = tasks.get(dest)
+        if not task:
+            await update.message.reply_text(_("任务不存在！"))
             return
-        task = tasks[dest]
         try:
-            task.trigger(bot)
+            # 手动触发任务
+            await task.trigger(context)
         except Exception as e:
-            logger.error(f'{message.from_user.id}：发生未知错误！执行任务失败。', exc_info=e)
-            bot.reply_to(message, _("发生未知错误！执行任务失败。"))
+            logger.error(f'{update.effective_user.id}：执行任务失败。', exc_info=e)
+            await update.message.reply_text(_("发生未知错误！执行任务失败。"))
 
 
-##################
-# Session 特有配置 #
-##################
-
-@bot.message_handler(commands=['set', 'get'])
-def session_config_handler(message):
-    """
-    设置 session 配置
-    :param message:
-    :return:
-    """
-    if not check_auth():
-        bot.reply_to(message, _("请先进行鉴权！"))
+async def session_config_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """设置或获取 session 配置"""
+    if not check_auth(update.effective_user.id):
+        await update.message.reply_text(_("请先进行鉴权！"))
         return
+    
+    is_set = update.message.text.startswith('/set')
+    command = '/set' if is_set else '/get'
 
-    # 解析指令
-    is_set = message.text.startswith('/set')
-    cmd: str = message.text[4:]
-    conf = cmd.split(maxsplit=1)
-    # 显示指令帮助
-    if len(conf) < 1:
+    if not context.args:
         desc = '\n'.join(v.make_help(k, is_set=is_set) for k, v in SESSION_CONFIG.items())
-        bot.reply_to(message, _("使用方法：\n{desc}").format(desc=desc))
+        await update.message.reply_text(_("使用方法：\n{desc}").format(desc=desc))
         return
-    # 获得指令对象
-    conf = conf[0]
-    if conf not in SESSION_CONFIG:
-        bot.reply_to(message, _("配置不存在！命令使用方法请参考 {cmd}")
-                     .format(cmd='/set' if is_set else '/get'))
+    
+    conf_key = context.args[0]
+    obj_conf = SESSION_CONFIG.get(conf_key)
+    if not obj_conf:
+        await update.message.reply_text(_("配置不存在！命令使用方法请参考 {cmd}").format(cmd=command))
         return
-    obj_conf = SESSION_CONFIG[conf]
-    # 获得参数
-    cmd = cmd[len(conf) + 1:]
-    # 调用
-    if is_set:
-        obj_conf.set(cmd, bot, message)
-    else:
-        obj_conf.get(cmd, bot, message)
+
+    value_str = ' '.join(context.args[1:])
+    try:
+        if is_set:
+            await obj_conf.set(value_str, update, context)
+        else:
+            await obj_conf.get(value_str, update, context)
+    except Exception as e:
+        logger.error(f'Session config handler failed for {conf_key}', exc_info=e)
+        await update.message.reply_text(_("处理配置时发生错误。"))
 
 
-#######
-# 交易 #
-#######
-
-
-@bot.message_handler(func=lambda m: True)
-def transaction_query_handler(message: Message):
-    """
-    交易语句处理
-    :param message:
-    :return:
-    """
-    if not check_auth():
-        auth_token_handler(message)
+async def transaction_query_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """交易语句或鉴权处理"""
+    user = update.effective_user
+    message = update.message
+    
+    if not check_auth(user.id):
+        auth_token = get_config('bot.auth_token')
+        if auth_token == message.text:
+            set_session(user.id, SESS_AUTH, True)
+            await message.reply_text(_("鉴权成功！"))
+        else:
+            await message.reply_text(_("鉴权令牌错误！"))
         return
-    # 已鉴权则处理交易
+    
     manager = get_manager()
     try:
-        # 准备交易上下文
-        tags = get_config('transaction.tags', []) + get_session(message.from_user.id, SESS_TX_TAGS, [])
-        # 处理交易
+        tags = get_config('transaction.tags', []) + get_session(user.id, SESS_TX_TAGS, [])
         tx_uuid, tx = manager.create_from_str(message.text, add_tags=tags)
-        # 创建消息按键
-        markup = InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton(_("撤回交易"), callback_data=f'withdraw:{tx_uuid}'))
-        # 回复
-        bot.reply_to(message, transaction.stringfy(tx), reply_markup=markup)
+        markup = InlineKeyboardMarkup([[InlineKeyboardButton(_("撤回交易"), callback_data=f'withdraw:{tx_uuid}')]])
+        await message.reply_text(transaction.stringfy(tx), reply_markup=markup)
     except ValueError as e:
-        logger.info(f'{message.from_user.id}：无法添加交易', exc_info=e)
-        bot.reply_to(message, e.args[0])
-    except OSError as e:
-        logger.error(f'{message.from_user.id}：账本文件读写错误！添加交易失败。', exc_info=e)
-        bot.reply_to(message, _("账本文件读写错误！添加交易失败。"))
+        logger.info(f'{user.id}：无法添加交易', exc_info=e)
+        await message.reply_text(e.args[0])
     except Exception as e:
-        logger.error(f'{message.from_user.id}：发生未知错误！添加交易失败。', exc_info=e)
-        bot.reply_to(message, _("发生未知错误！添加交易失败。"))
+        logger.error(f'{user.id}：添加交易失败。', exc_info=e)
+        await message.reply_text(_("发生未知错误！添加交易失败。"))
 
 
-@bot.callback_query_handler(func=lambda call: call.data[:8] == 'withdraw')
-def callback_withdraw(call: CallbackQuery):
-    """
-    交易撤回回调
-    :param call:
-    :return:
-    """
-    auth = get_session(call.from_user.id, SESS_AUTH, False)
-    if not auth:
-        bot.answer_callback_query(call.id, _("请先进行鉴权！"))
+async def callback_withdraw(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """交易撤回回调"""
+    query = update.callback_query
+    if not check_auth(query.from_user.id):
+        await query.answer(_("请先进行鉴权！"), show_alert=True)
         return
-    tx_uuid = call.data[9:]
+    
+    await query.answer()
+    tx_uuid = query.data[9:]
     manager = get_manager()
     try:
         manager.remove(tx_uuid)
-        # 修改原消息回复
-        message = _("交易已撤回")
-        code_format = MessageEntity('code', 0, len(message))
-        bot.edit_message_text(message,
-                              chat_id=call.message.chat.id,
-                              message_id=call.message.message_id,
-                              entities=[code_format])
+        message_text = _("交易已撤回")
+        entities = [MessageEntity(type=MessageEntity.CODE, offset=0, length=len(message_text))]
+        await query.edit_message_text(text=message_text, entities=entities)
     except ValueError as e:
-        logger.info(f'{call.id}：无法撤回交易', exc_info=e)
-        bot.answer_callback_query(call.id, e.args[0])
-    except OSError as e:
-        logger.error(f'{call.id}：账本文件读写错误！撤回交易失败。', exc_info=e)
-        bot.answer_callback_query(call.id, _("账本文件读写错误！撤回交易失败。"))
+        logger.info(f'{query.id}：无法撤回交易', exc_info=e)
+        await query.answer(e.args[0], show_alert=True)
     except Exception as e:
-        logger.error(f'{call.id}：发生未知错误！撤回交易失败。', exc_info=e)
-        bot.answer_callback_query(call.id, _("发生未知错误！撤回交易失败。"))
-
+        logger.error(f'{query.id}：撤回交易失败。', exc_info=e)
+        await query.answer(_("发生未知错误！撤回交易失败。"), show_alert=True)
 
 def serving():
     """
-    启动 Bot
-    :return:
+    启动 Bot 服务的主函数。由 main.py 调用。
     """
-
-    # 设置 Token
+    # 此函数假设所有配置（config, session等）已由 main.py 加载完毕
+    
+    # 创建 Application
     token = get_config('bot.token')
-    bot.token = token
-    # 设置代理
+    builder = Application.builder().token(token)
     proxy = get_config('bot.proxy')
-    if proxy is not None:
-        apihelper.proxy = {'https': proxy}
-    # 启动
-    bot.infinity_polling()
+    if proxy:
+        builder.proxy_url(proxy)
+    application = builder.build()
+
+    # 注册指令处理器
+    application.add_handler(CommandHandler("start", start_handler))
+    application.add_handler(CommandHandler("reload", reload_handler))
+    application.add_handler(CommandHandler("help", help_handler))
+    application.add_handler(CommandHandler("task", task_handler))
+    application.add_handler(CommandHandler(["set", "get"], session_config_handler))
+
+    # 注册回调查询处理器
+    application.add_handler(CallbackQueryHandler(callback_help, pattern=r'^help:'))
+    application.add_handler(CallbackQueryHandler(callback_withdraw, pattern=r'^withdraw:'))
+
+    # 注册消息处理器
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, transaction_query_handler))
+
+    # 注册定时任务
+    tasks = get_task()
+    job_queue = application.job_queue
+    logger.info("注册定时任务调度...")
+    # 您需要在这里根据您的业务逻辑实现任务调度
+    # for task_name, task_obj in tasks.items():
+    #     job_queue.run_daily(task_obj.trigger, time=..., name=task_name)
+    
+    logger.info("Bot polling astarted...")
+    application.run_polling()
