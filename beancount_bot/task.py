65756df3b7641d894a9d12f2ed016297f47598e9
sea-ten <58674672+sea-ten@users.noreply.github.com>
2025-09-28 10:30:50 +0800

    Update task.py

--- beancount_bot/task.py
+++ beancount_bot/task.py
@@ -1,14 +1,12 @@
-import threading
-import time
+import asyncio
 from typing import Dict
 
 import schedule
-from telebot import TeleBot
+from telegram.ext import ContextTypes
 
+# 注意： get_config, get_global, GLOBAL_TASK, logger, load_class 的导入保持不变
 from beancount_bot.config import get_config, get_global, GLOBAL_TASK
 from beancount_bot.util import logger, load_class
-
-_schedule_thread: threading.Thread = None
 
 
 class ScheduleTask:
@@ -22,19 +20,23 @@ class ScheduleTask:
         构造函数参数通过 **kwargs 形式传入
         """
         self.config = None
+        self.name = None
 
     def register(self, fire: callable):
         """
         注册定时任务。将在构造任务对象后立刻执行。如果不注册，将不会定时触发
         :param fire: 待执行函数
         """
+        # schedule 的注册逻辑可以保持，但调用方式会改变
         pass
 
-    def trigger(self, bot: TeleBot):
+    async def trigger(self, context: ContextTypes.DEFAULT_TYPE):
         """
         触发任务。任务可通过两种方式触发：定时执行（register 中注册）、/task 任务名
-        :param bot: Bot 对象
+        :param context: PTB 的上下文对象，可以通过 context.bot 访问机器人实例
         """
+        # 这里需要您根据具体任务来实现异步逻辑
+        # 例如： await context.bot.send_message(chat_id=..., text=...)
         pass
 
 
@@ -43,7 +45,7 @@ def load_task() -> Dict[str, ScheduleTask]:
     加载定时任务
     :return:
     """
-    from beancount_bot.bot import bot
+    # 这个函数不再需要导入 bot，也不再注册 schedule
     ret = {}
     schedule.clear()
     for conf in get_config('schedule', []):
@@ -51,11 +53,11 @@ def load_task() -> Dict[str, ScheduleTask]:
         clazz = load_class(conf['class'])
         args = conf['args']
 
-        logger.info('注册定时任务：%s', name)
+        logger.info('加载定时任务定义：%s', name)
         task: ScheduleTask = clazz(**args)
-        task.register(lambda capture_task=task: capture_task.trigger(bot))
+        # task.register(...) 的逻辑将移至 bot.py
         task.config = conf
-
+        task.name = name
         ret[name] = task
     return ret
 
@@ -67,26 +69,5 @@ def get_task() -> Dict[str, ScheduleTask]:
     """
     return get_global(GLOBAL_TASK, load_task)
 
-
-def start_schedule_thread(interval=5) -> threading.Thread:
-    """
-    运行定时任务
-    :param interval:
-    :return:
-    """
-
-    class ScheduleThread(threading.Thread):
-        @classmethod
-        def run(cls):
-            while True:
-                try:
-                    schedule.run_pending()
-                except Exception as e:
-                    logger.warn('定时任务出错：%s', e)
-                time.sleep(interval)
-
-    global _schedule_thread
-    _schedule_thread = ScheduleThread()
-    _schedule_thread.setDaemon(True)
-    _schedule_thread.start()
-    return _schedule_thread
+# start_schedule_thread 函数可以安全地删除了
+# def start_schedule_thread(...):
